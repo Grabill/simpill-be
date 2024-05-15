b725cb090424d596322e798f9f0534f08dab51e1
trietng <trietnguyen0781@gmail.com>
2024-05-15 22:40:49 +0700

    feat: supplement search

--- src/module/supplement/supplement.controller.ts
+++ src/module/supplement/supplement.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { SupplementService } from './supplement.service';
+import { SupplementQueryDto } from './dto/supplement-query.dto';
 
-@Controller('supplement')
+@Controller('supplements')
 export class SupplementController {
     constructor(private readonly supplementService: SupplementService) {}
 
     @Get()
-    async getSupplements() {
-        return this.supplementService.getFirstTenSupplements();
+    async getSupplements(@Query() query: SupplementQueryDto) {
+        console.log(query);
+        return await this.supplementService.findByName(query.q, query.exact);
     }
 }
--- src/module/supplement/supplement.service.ts
+++ src/module/supplement/supplement.service.ts
@@ -2,7 +2,7 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Supplement } from 'src/schema/supplement.schema';
-// import populate from 'src/util/populate';
+// import { populate } from 'src/util/database-helper';
 // import * as SupplementData from './data/splm_cleaned.json';
 
 @Injectable()
@@ -11,8 +11,18 @@ export class SupplementService {
         // populate(this.supplementModel, SupplementData as Supplement[]);
     }
 
-    async findByName(name: string) : Promise<Supplement> {
-        return this.supplementModel.findOne({ name: name.toUpperCase() });
+    /**
+     * Find a supplement by its name (case insensitive)
+     * @param name name of the supplement
+     * @param exact whether to match the name exactly
+     * @returns an array of supplements that match the name
+     */
+    async findByName(name: string, exact: boolean) : Promise<Supplement[]> {
+        name = name.toUpperCase();
+        if (exact) {
+            return await this.supplementModel.find({ name: name });
+        }
+        return await this.supplementModel.find({ name: { $regex: new RegExp(name) } });
     }
 
     async getFirstTenSupplements() : Promise<Supplement[]> {
