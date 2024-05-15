d5c672db79513c10fac6a84ea3b8547d89dd401a
trietng <trietnguyen0781@gmail.com>
2024-05-15 22:23:33 +0700

    refactor

--- src/util/database-helper.ts
+++ src/util/database-helper.ts
@@ -1,8 +1,14 @@
 import { Model } from 'mongoose';
 import { Logger } from '@nestjs/common';
 
-export default function populate<Schema>(model: Model<Schema>, data: Schema[]) {
-    const logger = new Logger(`Populator<${model.modelName}>`); 
+/**
+ * Function to populate a collection with data
+ * @param model the model to populate
+ * @param data the data to populate the collection witk
+ * @returns void
+ */
+export function populate<Schema>(model: Model<Schema>, data: Schema[]) {
+    const logger = new Logger(`[populate<${model.modelName}>`); 
     logger.log(`Populating ${model.modelName} collection...`);
     model.insertMany(data)
         .then(() => logger.log(`Successfully populated ${model.collection.name} collection!`))
