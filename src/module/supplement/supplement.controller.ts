adcf2628b6a3098be9eda1ba9b48359a174b00b8
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2024-05-16 11:22:24 +0700

    Create API for getting details of splm

--- src/module/supplement/supplement.controller.ts
+++ src/module/supplement/supplement.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { SupplementService } from './supplement.service';
 import { SupplementQueryDto } from './dto/supplement-query.dto';
 
@@ -9,5 +9,11 @@ export class SupplementController {
     @Get()
     async getSupplements(@Query() query: SupplementQueryDto) {
         return await this.supplementService.findByName(query.q, query.exact);
+    }
+
+    @Get(':name')
+    async getDetailedSupplementByName(@Param('name') name: string) {
+        const result = await this.supplementService.findByName(name, true);
+        return result.length > 0 ? result[0] : {};
     }
 }
