9277b34cfd359ca5ce5ab25af09eec3be20220de
dirii <minhquang116@gmail.com>
2024-05-20 19:13:24 +0700

    resolve conflict

--- suggestion/algorithms/bm25.py
+++ suggestion/algorithms/bm25.py
@@ -1,24 +1,15 @@
 from rank_bm25 import BM25Okapi
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from .suggestor import Suggestor
+from bs4 import BeautifulSoup
 import numpy as np
 import json, nltk
-<<<<<<< HEAD:suggestion/bm25.py
-from bs4 import BeautifulSoup
-=======
-from .suggestor import Suggestor
->>>>>>> fa216a611bf63295f24747f8deae55dc12385b97:suggestion/algorithms/bm25.py
 
 nltk.download('wordnet')
 wl = WordNetLemmatizer()
 
 
-<<<<<<< HEAD:suggestion/bm25.py
-class BM25:
-    def __init__(self, query, data): # data is a list of strings
-        self.query = self.processData(query.replace('effective use ', '').lower())
-        # self.data = self.cleanData(json.loads(data))
-=======
 class BM25(Suggestor):
     def __init__(self): # data is a list of strings
         self.data = []
@@ -27,7 +18,6 @@ class BM25(Suggestor):
 
     def loadData(self, data: list) -> None:
         # self.queryStr = self.processData(queryStr)
->>>>>>> fa216a611bf63295f24747f8deae55dc12385b97:suggestion/algorithms/bm25.py
         self.data = data
         self.tokenized_corpus = self.inputData(self.data)
         self.bm25 = BM25Okapi(self.tokenized_corpus)
