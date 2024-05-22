20698366b2f3bbd81351ee4dbfcd75b830b65ff7
trietng <trietnguyen0781@gmail.com>
2024-05-22 08:55:31 +0700

    update caching time

--- src/module/suggestion/suggestion.controller.ts
+++ src/module/suggestion/suggestion.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import { SuggestionService } from './suggestion.service';
 import { SuggestionQueryDto } from './dto/suggestion-query.dto';
 import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
-const CACHE_TTL = 60 * 10000; // 10 minutes
+const CACHE_TTL = 60 * 100000; // 60 minutes
 
 @Controller('suggestions')
 @CacheTTL(CACHE_TTL)
